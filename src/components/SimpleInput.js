aeb2e7bbe2c9f082b5aa1bd1be7dee1670152fca
Said-laasri <saidlaasri3@gmail.com>
2023-03-30 16:12:58 +0000

    Form submition with state/ref and simple validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,37 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [entredName, setEntredName] = useState("");
+  const nameRef = useRef();
+
+  const entredNmaeHundler = (e) => {
+    setEntredName(e.target.value);
+  };
+
+  const formSubmittion = (e) => {
+    e.preventDefault();
+
+    if( entredName.trim() === ''){
+      return;
+    }
+    
+    const entredValue = nameRef.current.value;
+    console.log(entredName, 'state');
+    console.log(entredValue, "ref");
+    setEntredName(''); //reset input with state
+    // nameRef.current.value = ''; //reset input with ref // not ideal // manipulate Dom 
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmittion}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameRef}
+          onChange={entredNmaeHundler}
+          type="text"
+          id="name"
+          value={entredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
